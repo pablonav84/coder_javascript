b8b38fcbde3a6bd3ec98a989d95ba7e86fb1502b
Pablo Navarro <pablonav84@gmail.com>
2023-09-30 10:11:55 -0300

    modificación mensaje bienvenida

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
-
-let entrada = prompt(`Ingrese una opcion para continuar o 0 para salir:
-1: Hamburguesa 2000
-2: Pizza 1000
-3: Lomito 2500
-4: Pancho 600
-5: Papas 800`);
-while (entrada != "0") {
-  switch (entrada) {
-    case "1":
-      console.log("Usted seleccionó Hamburguesa");
-      break;
-    case "2":
-      console.log("Usted seleccionó Pizza");
-      break;
-    case "3":
-      console.log("Usted seleccionó Lomito");
-      break;
-    case "4":
-      console.log("Usted seleccionó Pancho");
-      break;
-    case "5":
-      console.log("Usted seleccionó Papas");
-      break;
-    default:
-      alert("Por favor seleccione una opción valida");
-      break;
+alert("Bienvenido/a al Servicio de comidas")
+let respuesta = prompt("¿Desea realizar una compra? Presione s (si) o n (no)")
+while (respuesta != "n") {
+  if (respuesta == "s") {
+    let entrada = prompt(`Ingrese una opción para continuar o 0 para salir:
+    1: Hamburguesa
+    2: Pizza
+    3: Lomito
+    4: Pancho
+    5: Papas`);
+    let porcion = prompt("Ingrese Cantidad")
+    
+      switch (entrada) {
+        case "1":
+          console.log(`Usted seleccionó ${porcion} Hamburguesa`);
+          break;
+        case "2":
+          console.log(`Usted seleccionó ${porcion} Pizza`);
+          break;
+        case "3":
+          console.log(`Usted seleccionó ${porcion} Lomito`);
+          break;
+        case "4":
+          console.log(`Usted seleccionó ${porcion} Pancho`);
+          break;
+        case "5":
+          console.log(`Usted seleccionó ${porcion} Papas`);
+          break;
+        default:
+          alert("Por favor seleccione una opción valida");
+          break;
+      }
   }
-
-entrada = prompt(`Ingrese una opcion para continuar o 0 para salir:
-1: Hamburguesa 2000
-2: Pizza 1000
-3: Lomito 2500
-4: Pancho 800
-5: Papas 600`);
+  else {
+      alert("Respuesta Incorrecta");
+  }
+  respuesta = prompt("¿Desea realizar otra compra? Presione s (si) o n (no)")
 }
+console.log("¡Muchas Gracias!")
